418c53a327ce379f793ca9d839448f53f002db59
Artem Fedorov <fyod-artem@mail.ru>
2024-09-19 17:37:30 +0200

    Homework 10

---
+++ Homeworks/Homework 10/data.js
@@ -0,0 +1,64 @@
+const data = `
+[
+  {
+    "id": "1n7",
+    "url": "https://cdn2.thecatapi.com/images/1n7.jpg",
+    "width": 717,
+    "height": 960
+  },
+  {
+    "id": "32t",
+    "url": "https://cdn2.thecatapi.com/images/32t.gif",
+    "width": 500,
+    "height": 281
+  },
+  {
+    "id": "4ar",
+    "url": "https://cdn2.thecatapi.com/images/4ar.gif",
+    "width": 500,
+    "height": 281
+  },
+  {
+    "id": "5af",
+    "url": "https://cdn2.thecatapi.com/images/5af.jpg",
+    "width": 414,
+    "height": 310
+  },
+  {
+    "id": "812",
+    "url": "https://cdn2.thecatapi.com/images/812.gif",
+    "width": 241,
+    "height": 300
+  },
+  {
+    "id": "bre",
+    "url": "https://cdn2.thecatapi.com/images/bre.jpg",
+    "width": 640,
+    "height": 554
+  },
+  {
+    "id": "cnv",
+    "url": "https://cdn2.thecatapi.com/images/cnv.jpg",
+    "width": 640,
+    "height": 493
+  },
+  {
+    "id": "dj3",
+    "url": "https://cdn2.thecatapi.com/images/dj3.jpg",
+    "width": 480,
+    "height": 640
+  },
+  {
+    "id": "MTUwMDE5MA",
+    "url": "https://cdn2.thecatapi.com/images/MTUwMDE5MA.jpg",
+    "width": 630,
+    "height": 420
+  },
+  {
+    "id": "MTc3NjExMw",
+    "url": "https://cdn2.thecatapi.com/images/MTc3NjExMw.jpg",
+    "width": 484,
+    "height": 332
+  }
+]
+`
