11baaebf845ba6d5f727a0978851d449762077c4
Artem Fedorov <fyod-artem@mail.ru>
2024-09-25 21:38:31 +0200

    Homework 12

---
+++ Homeworks/Homework 12/data.js
@@ -0,0 +1,52 @@
+const dataProducts = `
+[
+    {
+        "name": "FALL JACKET",
+        "url": "img/image1.jpg",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "75",
+        "color": "Black",
+        "size": "XL"
+    },
+    {
+        "name": "THREE-PIECE SUIT",
+        "url": "img/image2.jpg",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "78",
+        "color": "Black",
+        "size": "M"
+    },
+    {
+        "name": "SWEATSHIRT",
+        "url": "img/image3.jpg",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52",
+        "color": "Blue",
+        "size": "L"
+    },
+    {
+        "name": "MEN'S PANTS",
+        "url": "img/image4.jpg",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "35",
+        "color": "Orange",
+        "size": "XL"
+    },
+    {
+        "name": "WOMEN'S JACKET",
+        "url": "img/image5.jpg",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "67",
+        "color": "Light blue",
+        "size": "M"
+    },
+    {
+        "name": "WOMEN'S BLOUSE",
+        "url": "img/image6.jpg",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "82",
+        "color": "Olive",
+        "size": "S"
+    }
+]
+`
